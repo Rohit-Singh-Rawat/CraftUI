2872b96f96ac8c257e5bdb03a7ec894c0577e57f
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2025-10-17 19:47:59 +0530

    fix: build

--- apps/web/components/playground/logo-demo.tsx
+++ apps/web/components/playground/logo-demo.tsx
@@ -1,7 +1,15 @@
-<div className='menu-wrapper flex py-10 flex-col items-center justify-center gap-4'>
-	<Logo className='w-32 h-32' />
-	<Logo className='w-24 h-24' />
-	<Logo className='w-20 h-20' />
-	<Logo className='w-16 h-16' />
-	<Logo className='w-12 h-12' />
-</div>;
+import Logo from '../layout/logo';
+
+const LogoDemo = () => {
+	return (
+		<div className='menu-wrapper flex py-10 flex-col items-center justify-center gap-4'>
+			<Logo className='w-32 h-32' />
+			<Logo className='w-24 h-24' />
+			<Logo className='w-20 h-20' />
+			<Logo className='w-16 h-16' />
+			<Logo className='w-12 h-12' />
+		</div>
+	);
+};
+
+export default LogoDemo;
